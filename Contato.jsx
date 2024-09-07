2e22a518847088ba56712d36279a5ba70bf77f57
Matheus Taylor Dev <142901011+Slayer20XX@users.noreply.github.com>
2024-09-07 20:36:15 -0300

    Add files via upload

---
+++ Contato.jsx
@@ -0,0 +1,17 @@
+const Contato=()=>{
+    return(
+        <>
+            <h1>Pagina Contato</h1>
+            <p>
+            Telefone: (11)9844-8201
+            <br/>
+            Email: Aventura_Esportiva@gmail.com
+            <br/>
+            Endereço:Rua das Palmeiras, 123
+            <br/>
+            Horário de Funcionamento: Seg-Sex: 9h-18h
+            </p>
+        </>
+    )
+}
+export default Contato
